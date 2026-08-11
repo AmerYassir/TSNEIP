233a6b5c759dbb727b6f896f8742a75ad98744c3
Amer Abduljalil <amer.yassir.abdaljalil@gmail.com>
2026-08-11 11:37:05 +0300

    Refactor observations app and add new migrations
    - Renamed ObservationConfig to ObservationsConfig and set default_auto_field.
    - Implemented a ready method to import signals in the observations app.
    - Created a new migration for AdministrativeUnit model to add Arabic and English name fields, and updated existing fields.
    - Added a migration for GeoObservation and ObservationSubdomain models to alter options and fields.
    - Introduced signals to automatically assign the administrative unit based on the observation's location.
    - Added tests for the new signal functionality to ensure correct administrative unit assignment.
    - Created initial migration for the Organization model with necessary fields and options.
    - Added CSS and JS files for tabbed translation fields in the admin interface.

--- backend/code/observations/apps.py
+++ backend/code/observations/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
 
-class ObservationConfig(AppConfig):
+class ObservationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'observations'
+
+    def ready(self):
+        import observations.signals  # noqa: F401
